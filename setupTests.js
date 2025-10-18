2d86cc2340bdf14d71f285b7c9c1a76454ca5fab
Kyle McColgan <kdmbus21@gmail.com>
2025-10-18 00:11:08 -0500

    setupTests.js: Force the GitHub test runner to use the same timezone as my machine.

---
+++ setupTests.js
@@ -0,0 +1,8 @@
+//Filename: setupTests.js
+//Author: Kyle McColgan
+//Date: 17 October 2025
+//Description: This file contains Jest test set up for the GitHub Actions test workflow.
+
+import '@testing-library/jest-dom';
+
+process.env.TZ = 'America/Chicago';
